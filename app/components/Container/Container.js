2a68264d6485b5aefd0e0e09c4ab779867a6538e
mattperkins <github@mattperkins.com>
2018-03-19 09:56:04 +0000

    add component

---
+++ app/components/Container/Container.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View } from 'react-native'
+
+
+const Container = ({children}) => (
+  <View> 
+     {children} 
+  </View>
+)
+
+Container.propTypes = {
+ children: PropTypes.element,
+}
+
+export default Container
